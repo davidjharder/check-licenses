62b60e4b7dbf73db22bc0e2d0f9746b05064f5f1
David Harder <david@davidjharder.ca>
2023-11-19 19:14:47 -0600

    Fix spelling and other nits

--- check-licenses.go
+++ check-licenses.go
@@ -1,4 +1,4 @@
-// Checks the license key(s) of package.yml files against the list of SPDX identifiers.
+// Checks the license key(s) of all package.yml files against the list of SPDX identifiers.
 // Assumes you are running from the root of the package monorepo.
 
 package main
@@ -18,8 +18,9 @@ import (
 )
 
 const licenseListJSONsource = "https://spdx.org/licenses/licenses.json"
-const licenseListJSONfile = "common/licenses.json"
+const licenseListJSONfile = "common/Go/check-licenses/licenses.json"
 
+// type to hold parsed license JSON data
 // https://mholt.github.io/json-to-go/
 type licensesData struct {
 	LicenseListVersion string `json:"licenseListVersion"`
@@ -37,6 +38,7 @@ type licensesData struct {
 	ReleaseDate string `json:"releaseDate"`
 }
 
+// types to hole the parsed yaml data, where license keys come in two flavors
 type yamlDataSlice struct {
 	License []string `yaml:"license"`
 }
@@ -57,7 +59,6 @@ func main() {
 			log.Fatal(err)
 		}
 	}
-
 	// Open the licenses.json file and un-marshall it
 	JSONfileContent, err := os.ReadFile(licenseListJSONfile)
 	if err != nil {
@@ -68,7 +69,6 @@ func main() {
 	if err != nil {
 		log.Fatal("JSON Un-marshall error: ", err)
 	}
-
 	// Put all the valid identifiers in a list
 	var validIdentifiers []string
 	for _, entry := range licensesJSON.Licenses {
@@ -76,10 +76,11 @@ func main() {
 			validIdentifiers = append(validIdentifiers, entry.LicenseID)
 		}
 	}
-	// Walk the "package" directory, passing any found package.ymls off to the checker
+	// Walk the "packages" directory, passing any found package.yml files off to the checker
 	err = filepath.Walk("packages", func(path string, info fs.FileInfo, err error) error {
 		if err != nil {
-			log.Fatal("File walk error: ", err)
+			fmt.Println("File walk error at path: ", path)
+			return err
 		}
 		//fmt.Println(path)
 		if info.Name() == "package.yml" {
@@ -87,26 +88,29 @@ func main() {
 		}
 		return nil
 	})
+	if err != nil {
+		log.Fatal("File walk error: ", err)
+	}
 }
 
-// The checker functions
-// Yaml is parsed twice because the license key can either contain a single string or a list of strings
+// The checker function
+// Yaml file is parsed twice because the license key can either contain a single string or a list of strings
 func licenseCheck(yamlPath string, licenseList []string) {
 	YAMLfileContent, err := os.ReadFile(yamlPath)
 	if err != nil {
-		log.Fatal("Error when opening file: ", err)
+		log.Fatal("Error when opening file: ", yamlPath, err)
 	}
 	var yamlParseString yamlDataString
 	var yamlParseSlice yamlDataSlice
 
-	// Try parsing the yaml with a single string at the license key
+	// Try parsing the yaml file with a single string at the license key
 	err = yaml.Unmarshal([]byte(YAMLfileContent), &yamlParseString)
 
 	// if it errors, assume it is because the license is a list
 	if err != nil {
 		//fmt.Println("Switching to multi-license parsing")
 		err = yaml.Unmarshal([]byte(YAMLfileContent), &yamlParseSlice)
-		if err !=nil {
+		if err != nil {
 			log.Fatal("Error parsing multiline: ", err)
 		}
 		for _, id := range yamlParseSlice.License {
@@ -114,10 +118,10 @@ func licenseCheck(yamlPath string, licenseList []string) {
 				//fmt.Println("ok: ", yamlPath, " ", id)
 			} else {
 				fmt.Println("BAD: ", yamlPath, " ", id)
-				
+
 			}
 		}
-		return // return early for yamls with license list
+		return // return early for yaml files with license *list*
 	}
 	if slices.Contains(licenseList, yamlParseString.License) {
 		//fmt.Println("ok: ", yamlPath, " ", yamlParseString.License)
